ed6b942b2ade775c002831a93af345d52471dcc4
Siddhant <forsomethingnewsid@gmail.com>
2024-07-12 21:40:10 +0000

    TRY

---
+++ run.sh
@@ -0,0 +1,7 @@
+conda create -n fl_env python=3.10 anaconda
+conda activate fl_env
+pip install -r requirements.txt
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+git clone https://github.com/data-science-lover/flower.git
+cd flower
+pip install .
