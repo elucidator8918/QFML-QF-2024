cf94f55c219c818838787ae4bef507fd397e5724
Siddhant <forsomethingnewsid@gmail.com>
2024-07-13 00:56:01 +0000

    Standard + FHE DONE

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # QFML-QF-2024
 
 conda create -n fed python=3.10 anaconda
+
+close your terminal
+
 conda activate fed
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-pip install -r requirements.txt
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements-cpu.txt
 git clone https://github.com/data-science-lover/flower.git
 cd flower
 pip install .
